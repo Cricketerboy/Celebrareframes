6189ee770d3053eef260486fe23411f16643e97f
Mohit Anand <praveenanand801@gmail.com>
2023-08-21 17:58:35 +0530

    first commit

---
+++ lib/splashscreen.dart
@@ -0,0 +1,46 @@
+import 'package:celebrareclone/home.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int splashtime = 3;
+  // duration of splash screen on second
+
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: splashtime), () async {
+      Navigator.pushReplacement(context, MaterialPageRoute(
+          //pushReplacement = replacing the route so that
+          //splash screen won't show on back button press
+          //navigation to Home page.
+          builder: (context) {
+        return Home();
+      }));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          alignment: Alignment.center,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Text(
+            "Celebrare",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.black45,
+            ),
+          )),
+    );
+  }
+}
